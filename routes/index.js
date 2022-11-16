b019a09845e679819df4bfcaf806a45fbfcd24fe
jpurvis2 <jpurvis2@email.unc.edu>
2022-11-16 11:53:43 -0500

    a04

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+const { roll } = require("../lib/roll.js");
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.statusCode(200);
+  res.send();
+});
+
+router.get('/roll', function(req, res) {
+  let sides = req.query.sides 
+  let dice = req.query.dice
+  let rolls = req.query.rolls
+
+  if (sides) {
+    sides = parseInt(sides);
+  } else {
+    sides = 6
+  }
+  if (dice) {
+    dice = parseInt(dice);
+  } else {
+    dice = 2
+  }
+  if (rolls) {
+    rolls = parseInt(rolls);
+  } else {
+    rolls = 1
+  }
+
+  res.status(200).json(
+    roll(
+      sides,dice,rolls
+    )
+  )
+});
+router.get('/roll/:sides', function(req, res) {
+  const sides = req.params.sides;
+  res.status(200).json(
+    roll(
+      sides,2,1
+    )
+  )
+})
+router.get('/roll/:sides/:dice', function(req, res) {
+  const sides = req.params.sides;
+  const dice = req.params.dice;
+  res.status(200).json(
+    roll(
+      sides,dice,1
+    )
+  )
+})
+router.get('/roll/:sides/:dice/:rolls', function(req, res) {
+  const sides = req.params.sides;
+  const dice = req.params.dice;
+  const rolls = req.params.rolls;
+  res.status(200).json(
+    roll(
+      sides,dice,rolls
+    )
+  )
+})
+module.exports = router;
